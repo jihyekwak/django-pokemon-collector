988911827838ca5750833f66f0c74c5aea935db6
jihyekwak <jihyekwak53@gmail.com>
2022-03-19 16:06:53 -0700

    create django template and update view

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpRequest
 from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+# from django.views import View
+# from django.http import HttpResponse
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Pokemon Collector")
+# class Home(View):
+#     def get(self, request):
+#         return HttpResponse("Pokemon Collector")
+
+class Home(TemplateView):
+    template_name='home.html'
