ef8732f3b518f33b9093e89565b4ee680b061f1e
jihyekwak <jihyekwak53@gmail.com>
2022-03-19 16:21:32 -0700

    add index page

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,20 @@ from django.views.generic.base import TemplateView
 
 class Home(TemplateView):
     template_name='home.html'
+
+class Pokemon:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+pokemons = [
+    Pokemon("Pikachu", "Electric")
+]
+
+class Index(TemplateView):
+    template_name='index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pokemons']= pokemons
+        return context
