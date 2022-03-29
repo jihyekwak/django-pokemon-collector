d0b7d88cf1efe47af7a3f4018253bf7ff2a44653
jihyekwak <jihyekwak53@gmail.com>
2022-03-29 15:41:38 -0700

    add styles

--- main_app/views.py
+++ main_app/views.py
@@ -13,41 +13,11 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-# from django.views import View
-# from django.http import HttpResponse
-
-# class Home(View):
-#     def get(self, request):
-#         return HttpResponse("Pokemon Collector")
-
 class Home(TemplateView):
     template_name='home.html'
 
 class About(TemplateView):
     template_name='about.html'
-
-# class Pokemon:
-#     def __init__(self, name, type, description, abilities=[],is_collected = False ):
-#         self.name = name
-#         self.type = type
-#         self.description = description
-#         self.abilities = abilities
-#         self.is_collected = is_collected
-
-# pokemons = [
-#     Pokemon("Pikachu", "Electric", 'Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.', ['Static'], True ),
-#     Pokemon("Eevee", "Normal", 'Eight different Pokemon evolve from the amazingly adaptive Eevee, accrogind to current stuies. It has the ability to alter the composition of its body to suit its surrounding environment.', ['Run Away', 'Adaptability'], False ),
-#     Pokemon("Togepi", "Normal", 'The shell seems to be filled with joy. It is said that it will share good luck when treated kindly.', ['Serene Grace', 'Hustle'], True ),
-#     Pokemon("Clefairy", "Fairy", 'It is said that happiness will come to those who see a gathering of Clefairy dancing under a full moon.', ['Cute Charm', 'Nagic Guard'], True),
-#     Pokemon("Snorlax", "Normal", 'It is not satisfied unless it eats over 880 pounds of food every day. When it is done eating, it goes promptly to sleep.', ['Thick Fat', 'Immunity'], True),
-#     Pokemon("Charizard", "Fire", 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.', ['Blaze'], True),
-#     Pokemon("Piplup", "Water", 'It doesn’t like to be taken care of. It’s difficult to bond with since it won’t listen to its Trainer.', ['Torrent'], True)
-# ]
-
-# collected_pokemons = [pokemon for pokemon in pokemons if pokemon.is_collected]
-
-
-# Pokemon
 
 class Index(TemplateView):
     template_name='index.html'
@@ -57,7 +27,7 @@ class Index(TemplateView):
         name = self.request.GET.get("name")
         if name != None:
             context['pokemons'] = Pokemon.objects.filter(name__icontains=name)
-            context['header'] = f"Searching for {name}"
+            context['header'] = f"Searching for {name.capitalize()}"
         else:
             context['pokemons']= Pokemon.objects.all()
             context['header']= "Index of Pokemon"
@@ -78,7 +48,7 @@ class Delete(DeleteView):
 @method_decorator(login_required, name='dispatch')
 class Create(CreateView):
     model = Pokemon
-    fields = ['name', 'img', 'type', 'description','gender', 'evolved', 'user']
+    fields = ['name', 'img', 'type', 'description','abilities', 'gender', 'evolved', 'user']
     template_name= "create.html"
     success_url ="/index/"
 
@@ -95,7 +65,6 @@ class Update(UpdateView):
     template_name = "update.html"
     def get_success_url(self):
         return reverse('detail', kwargs = {'pk': self.object.pk})
-
 
 # User
 @login_required
