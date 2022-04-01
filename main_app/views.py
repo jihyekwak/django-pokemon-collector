d2b82d223831cde1cee923fd9e80172c89132233
jihyekwak <jihyekwak53@gmail.com>
2022-04-01 01:58:04 -0700

    add fonts file and update styles.css

--- main_app/views.py
+++ main_app/views.py
@@ -103,17 +103,11 @@ class Move_Update(UpdateView):
     template_name = 'move_update.html'
     success_url = "/moves/"
 
-    # def get_queryset(self):
-    #     return Move.objects.all()
-
 @method_decorator(login_required, name='dispatch')
 class Move_Delete(DeleteView):
     model = Move
     template_name = 'move_delete_confirmation.html'
     success_url = "/moves/"
-
-    def get_queryset(self):
-        return Move.objects.all()
 
 # League
 
